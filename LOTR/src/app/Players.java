e64e02421114326fdcaf6e4d2ca1773e8dfece80
estudioddai <estudiodortiz@gmail.com>
2020-04-22 21:57:24 -0300

    commit 22/4

--- LOTR/src/app/Players.java
+++ LOTR/src/app/Players.java
@@ -5,6 +5,8 @@ import personajes.Personaje;
 public class Players  {
 
     private Personaje personaje;
+	public String nombre;
+	public int salud;
 
     public void atacar(){
     this.personaje.atacar;
--- LOTR/src/juego/Players.java
+++ LOTR/src/juego/Players.java
@@ -1,4 +1,4 @@
-package juego;
+
 
 public class Players {
 
